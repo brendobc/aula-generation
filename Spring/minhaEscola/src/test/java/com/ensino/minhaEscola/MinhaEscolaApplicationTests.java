573162214bd6509927442967aedcd30d53306b8b
unknown <brendo.cliber@gmail.com>
2020-09-04 13:14:50 -0300

    Organizando o repositorio

---
+++ Spring/minhaEscola/src/test/java/com/ensino/minhaEscola/MinhaEscolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensino.minhaEscola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaEscolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
